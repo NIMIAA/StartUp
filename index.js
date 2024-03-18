f90f625e2f0e991f62c3516bde06c9e8c4352617
Ayomide Aderotoye <ayomide.aderotoye@gmail.com>
2024-03-18 17:41:35 +0000

    new

--- index.js
+++ index.js
@@ -15,51 +15,13 @@ confirm_password.addEventListener("keyup", validatePassword);
 
 const link = document.getElementById('link');
 
-// var username = document.getElementById('username').value;
-// var text = document.getElementById('new-text');
-
-// text.textContent = username
-
-// text.innerHTML = username.value + 'profile'
-// var text = document.getElementById('new-text').innerHTML
-
 
 const input = document.getElementById('input-text');
 
 function autoExpand(){
-    // this.style.width = 'auto'
-    // this.style.width = input.scrollWidth + 'px'
     input.style.height = 'auto';
     input.style.height = input.scrollHeight + 'px'
 }
 input.addEventListener('input', autoExpand);
 
-// let input = document.getElementById("inputTag");
-// let fileInput = document.querySelector(".file-input");
-// let imageName = document.getElementById("imageName");
 
-// fileInput.addEventListener("click", function(){
-//     input.click();
-// });
-
-// input.addEventListener("change", function(){
-//     let fileName = input.files[0].name;
-//     fileInput.textContent = fileName;    
-// });
-
-// let inputText = document.querySelector("#inputText");
-// let list = document.getElementById("list");
-
-// inputText.addEventListener("keyup", function(event){
-//     if(event.key == "Enter"){
-//         addItem(this.value);
-//         this.value = "";
-//     }
-// });
-
-// let addItem = (inputText) => {
-//     let listItem = document.createElement("li");
-//     listItem.innerHTML = inputText;
-
-//     list.appendChild(listItem);
-// };
